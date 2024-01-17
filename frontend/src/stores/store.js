1ccdafea8aa6a3e1543a7abee880b714bcfff216
ivanterzic <ivan.terzic@fer.hr>
2024-01-17 17:08:34 +0100

    deploy edit

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -8,12 +8,19 @@ export const useStore = defineStore({
   }),
   actions: {
     async fetchCities() {
-      //const response = await axios.get('http://localhost:3000/cities')
-      const response = await axios.get('http://localhost:3000/cities')
+      
+      /*const response = await axios.get('http://localhost:3000/cities')
       this.cities = response.data.cities.map(city => ({
         ...city,
         population: parseInt(city.population),
+      }))*/
+
+      const response = await require('../../../city_population.json')
+      this.cities = response.cities.map(city => ({
+        ...city,
+        population: parseInt(city.population),
       }))
+      
     },
   },
 })
